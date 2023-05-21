46b32982d550f38f7f145a60eea3736e882ad622
M-Farag <mina.farag@icloud.com>
2023-05-21 21:49:03 +0300

    ADD Process replace text functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use clap::Parser;
 use crate::task::{TaskOperation, TaskList, Task};
-use std::{fs, io::{BufReader, BufWriter}};
+use std::{fs, io::{BufReader, BufWriter, BufRead}};
+use regex::Regex;
+use std::io::Write;
 
 pub mod task;
 
@@ -48,7 +50,7 @@ impl Runner {
                 if let Some(task_operation) = task.operation.as_ref() {
                     match task_operation {
                         TaskOperation::Replace => {
-                            println!("Replace in task: {:#?}", task);
+                            process_replace_task(task);
                         },
                         TaskOperation::Count => {
                             println!("Count in task: {:#?}", task);
@@ -57,6 +59,23 @@ impl Runner {
                 }
             }
         );
+
+        fn process_replace_task(task: &Task) {
+            let input_file = fs::File::open("tmp_input.txt").unwrap();
+            let input_file_buf_read = BufReader::new(input_file);
+            
+            let output_file = fs::File::create("tmp_output.txt").unwrap();
+            let mut output_file_buf_write = BufWriter::new(output_file);
+
+            let pattern = Regex::new(&task.data[0]).unwrap();
+            for line in input_file_buf_read.lines() {
+                let line = line.unwrap();
+                let line_replaced = pattern.replace_all(&line, &task.data[1]);
+                writeln!(output_file_buf_write,"{}",line_replaced).unwrap();
+            }
+
+            fs::rename("tmp_output.txt", "tmp_input.txt").unwrap();
+        }
     }
 }
 
